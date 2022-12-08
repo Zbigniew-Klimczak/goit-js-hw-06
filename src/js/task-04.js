e2bf6e019dafc96daf29da0c32d872f0812e002a
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-08 23:44:25 +0100

    Done

---
+++ src/js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0;
+let span = document.querySelector("#value");
+const incButton = document.querySelector(`button[data-action="increment"]`);
+const decButton = document.querySelector(`button[data-action="decrement"]`);
+console.log(incButton);
+console.log(decButton);
+incButton.addEventListener("click", () => {
+  counterValue += 1;
+  span.textContent = counterValue;
+});
+decButton.addEventListener("click", () => {
+  counterValue -= 1;
+  span.textContent = counterValue;
+});
