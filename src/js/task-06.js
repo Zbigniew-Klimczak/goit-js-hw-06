bd50537d0ae68db51bbb8bd3f2bf8011c52f475a
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-11 23:25:35 +0100

    Updated

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#validation-input");
 input.addEventListener("blur", () => {
-  if (input.value.length >= input.dataset.length) {
+  if (input.value.length == input.dataset.length) {
     input.classList.add("valid");
     input.classList.remove("invalid");
   } else {
