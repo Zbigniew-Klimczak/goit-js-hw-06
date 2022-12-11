2f39f48a6ad56c09773b30387830c57b5a153bf3
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-11 23:18:56 +0100

    Updated

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -3,8 +3,8 @@ const output = document.querySelector("#name-output");
 console.log(input);
 console.log(output);
 input.addEventListener("input", (event) => {
-  if (event.currentTarget.value) {
-    output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.trim()) {
+    output.textContent = event.currentTarget.value.trim();
   } else {
     output.textContent = "Anonymous";
   }
