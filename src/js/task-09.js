bd91cb51b53ecdc5c5eac0d2c6390af1d5eb1792
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-11 22:03:50 +0100

    Done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorChangeButton = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const body = document.querySelector("body");
+colorChangeButton.addEventListener("click", colorChange);
+function colorChange() {
+  const randomColor = getRandomHexColor();
+  spanColor.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+}
