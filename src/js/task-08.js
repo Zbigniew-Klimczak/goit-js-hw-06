db49ac95efcf19ce723a781d33116ae7195ff955
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2022-12-11 21:38:51 +0100

    Done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", submitForm);
+function submitForm(evt) {
+  evt.preventDefault();
+  const {
+    elements: { email, password },
+  } = evt.currentTarget;
+  if (email.value === "" || password.value === "") {
+    window.alert("Please fill in all the fields!");
+  } else {
+    const formObject = new Object();
+    formObject.email = email.value;
+    formObject.password = password.value;
+    console.log(formObject);
+    evt.currentTarget.reset();
+  }
+}
